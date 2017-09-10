20a2f4d8528ce6773281be78246f0786dece5397
Danny Boy Noynay <danoynay@up.edu.ph>
2017-09-10 23:29:50 +0800

    Update JeepneyTrip.java

--- JeepneyTrip.java
+++ JeepneyTrip.java
@@ -5,6 +5,9 @@ class JeepneyTrip {
   
   // constants
   final int BASE_KM = 5;
+  final double BASE_FARE = 7.0;
+  final double BASE_DISCOUNT = 6.0;
+  final double EXCESS_FARE = 0.5;
   
   
   JeepneyTrip(double distance, int totalPassengers, int discountPassengers) {
@@ -13,7 +16,37 @@ class JeepneyTrip {
     this.discountPassengers = discountPassengers;
   }
   
-  double fareChange(double payment) {
-    return 0;
+  // double -> double
+  // Returns a value if there is an exccess distance or more than the 
+  // base distance which is 5 kilometers. Returns zero if there is non.
+  double excessDistance(double distance) {
+    if (distance <= BASE_KM) {
+      return 0;
+    } else {
+      return distance - BASE_KM;
+    }
+  } 
+  
+  // int, double, int -> double 
+  // returns the computed fare of passengers considering 
+  //its type (regular or discounted passengers).  
+  double computeFare(int passengers, double fare) {
+  return passengers * (fare + (excessDistance(distance) * EXCESS_FARE)) ;
   }
+  
+  // -> double 
+  // Returns the total fare, given the total payment of passengers 
+  // including the discounted passengers.
+  double totalFare() {
+  return computeFare(totalPassengers - discountPassengers, BASE_FARE)
+    + computeFare(discountPassengers, BASE_DISCOUNT);
+  }
+  
+  // double -> double
+  // Returns the change, given the payment fare, total number of passengers,
+  // number of passengers with discount, and the trip distance.
+  double fareChange(double payment) {
+    return payment - totalFare();
+  }
+  
 }
