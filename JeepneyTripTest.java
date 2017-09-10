bf1b77d488987f3c7e65d7daa0bb1f3d9ac43307
Danny Boy Noynay <danoynay@up.edu.ph>
2017-09-10 23:30:42 +0800

    Update JeepneyTripTest.java

--- JeepneyTripTest.java
+++ JeepneyTripTest.java
@@ -13,6 +13,9 @@ public class JeepneyTripTest extends TestCase {
    * many "testSomething" methods in this class as you wish, and each
    * one will be called when running JUnit over this class.)
    */
+  
+  // Constructor: JeepneyTrip(double distance, int totalPassengers, int discountPassengers)
+  
   public void testTripCreated() {
     JeepneyTrip t = new JeepneyTrip(5, 5, 3);
     
@@ -21,15 +24,26 @@ public class JeepneyTripTest extends TestCase {
     assertEquals(3, t.discountPassengers);
   }
   
-  public void testChangeNoDiscount() {
-    JeepneyTrip t = new JeepneyTrip(5, 1, 0);
+  public void testFareChange() {
+    JeepneyTrip t1 = new JeepneyTrip(4.5, 1, 0);
+    JeepneyTrip t2 = new JeepneyTrip(4.5, 1, 1);
+    JeepneyTrip t3 = new JeepneyTrip(4.5, 2, 1);
+    JeepneyTrip t4 = new JeepneyTrip(5, 1, 0);
+    JeepneyTrip t5 = new JeepneyTrip(5, 1, 1);
+    JeepneyTrip t6 = new JeepneyTrip(5, 2, 1);
+    JeepneyTrip t7 = new JeepneyTrip(5.5, 1, 0);
+    JeepneyTrip t8 = new JeepneyTrip(5.5, 1, 1);
+    JeepneyTrip t9 = new JeepneyTrip(5.5, 2, 1);
     
-    assertEquals(3.0, t.fareChange(10));
+    assertEquals(3.0, t1.fareChange(10));
+    assertEquals(4.0, t2.fareChange(10));
+    assertEquals(7.0, t3.fareChange(20));
+    assertEquals(3.0, t4.fareChange(10));
+    assertEquals(4.0, t5.fareChange(10));
+    assertEquals(7.0, t6.fareChange(20));
+    assertEquals(2.75, t7.fareChange(10));
+    assertEquals(3.75, t8.fareChange(10));
+    assertEquals(6.5, t9.fareChange(20));
   }
   
-  public void testChangeDiscount() {
-    JeepneyTrip t = new JeepneyTrip(5, 1, 1);
-    
-    assertEquals(4.0, t.fareChange(10));
-  }
 }
